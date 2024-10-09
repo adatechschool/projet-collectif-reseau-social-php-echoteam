64b65b295813772a6059d941325905641139b6b0
solena8 <solena_ananda@hotmail.com>
2024-10-09 16:44:59 +0200

    login et log out fonctionnent - poster sur le mur en cours

---
+++ login.php
@@ -0,0 +1,92 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html lang="fr">
+<?php
+include 'var_globale.php';
+
+echo $head;
+?>
+
+        <div id="wrapper" >
+
+            <aside>
+                <h2>Présentation</h2>
+                <p>Bienvenu sur notre réseau social.</p>
+            </aside>
+            <main>
+                <article>
+                    <h2>Connexion</h2>
+                    <?php
+                    // vérifie si on est en train d'afficher ou de traiter le formulaire
+                    // si on recoit un champs email rempli il y a une chance que ce soit un traitement
+                    $enCoursDeTraitement = isset($_POST['email']);
+                    if ($enCoursDeTraitement)
+                    {
+                        // on ne fait ce qui suit que si un formulaire a été soumis.
+                        // récupère ce qu'il y a dans le formulaire
+                        $emailAVerifier = $_POST['email'];
+                        $passwdAVerifier = $_POST['motpasse'];
+
+                        //Petite sécurité pour éviter les injections sql
+                        $emailAVerifier = $mysqli->real_escape_string($emailAVerifier);
+                        $passwdAVerifier = $mysqli->real_escape_string($passwdAVerifier);
+                        // on crypte le mot de passe pour éviter d'exposer notre utilisatrice en cas d'intrusion dans nos systèmes
+                        $passwdAVerifier = md5($passwdAVerifier);
+                        //construction de la requete
+                        $lInstructionSql = "SELECT * "
+                                . "FROM users "
+                                . "WHERE "
+                                . "email LIKE '" . $emailAVerifier . "'"
+                                ;
+                        // Vérification de l'utilisateur
+                        $res = $mysqli->query($lInstructionSql);
+                        $user = $res->fetch_assoc();
+                        if ( ! $user OR $user["password"] != $passwdAVerifier)
+                        {
+                            echo "La connexion a échouée. ";
+                            
+                        } else
+                        {
+                            echo "Votre connexion est un succès : " . $user['alias'] . ".";
+                            // Se souvenir que l'utilisateur s'est connecté pour la suite
+                            // documentation: https://www.php.net/manual/fr/session.examples.basic.php
+                            $_SESSION['connected_id']=$user['id'];
+                            // Redirection après connexion réussie vers le feed
+                            header("Location: feed.php"); // Redirige vers une page protégée
+                            exit();
+                        }
+                    }
+
+
+        if (isset($_SESSION['connected_id'])) {
+            // L'utilisateur est connecté, afficher le bouton de déconnexion
+            echo '<form action="logout.php" method="post">';
+            echo '<input type="submit" value="Déconnexion">';
+            echo '</form>';
+        } else {
+            // L'utilisateur n'est pas connecté, afficher le formulaire de connexion
+            ?>
+                    <form action="login.php" method="post">
+                        <dl>
+                            <dt><label for='email'>E-Mail</label></dt>
+                            <dd><input type='email' name='email' aria-describedby='email-error'></dd>
+                            <dt><label for='motpasse'>Mot de passe</label></dt>
+                            <dd><input type='password' name='motpasse' aria-describedby='motpasse-error'></dd>
+                        </dl>
+                        <input type='submit' value="Connexion">
+                    </form>
+                    <p>
+                        Pas de compte?
+                        <a href='registration.php'>Inscrivez-vous.</a>
+                    </p>
+                    <?php
+                    }
+                    ?>
+                </article>
+            </main>
+
+        </div>
+    </body>
+</html>
--- wall.php
+++ wall.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+if (!isset($_SESSION['connected_id'])) {
+    // Si l'utilisateur n'est pas connecté, redirige vers la page de connexion
+    header("Location: login.php");
+    exit();
+}
+$userId = $_SESSION['connected_id'];
+?>
 <!doctype html>
 <html lang="fr">
 <?php
@@ -6,6 +15,7 @@ echo $head;
 ?>
 
         <div id="wrapper">
+
             <aside>
 
                 <?php
@@ -23,8 +33,67 @@ echo $head;
                 </section>
             </aside>
             <main>
+                <article>
+                    <h2>Poster un message</h2>
+                    <?php
+                    $laQuestionEnSql = "SELECT * FROM users";
+                    $lesInformations = $mysqli->query($laQuestionEnSql);
+                    while ($user = $lesInformations->fetch_assoc())
+                    {
+                        $listAuteurs[$user['id']] = $user['alias'];
+                    }
 
-                <?php
+                    $enCoursDeTraitement = isset($_POST['auteur']);
+                    if ($enCoursDeTraitement) {
+                        // on ne fait ce qui suit que si un formulaire a été soumis.
+                        // On récupère ce qu'il y a dans le formulaire
+                        $authorId = $_SESSION['connected_id'];
+                        $postContent = $_POST['message'];
+
+                        // Verification des champs et création des messages d'erreur éventuels
+                        $erreurs = [];
+                        if (empty($postContent)) {
+                            $erreurs[] = "Le message est requis.";
+                        }
+
+                        if (count($erreurs) === 0) { //(si aucune erreur n'est inscrite dans le tableau de message d'erreur, le code s'execute)
+                            //Petite sécurité pour éviter les injections sql
+                            $authorId = intval($mysqli->real_escape_string($authorId));
+                            $postContent = $mysqli->real_escape_string($postContent);
+                            //construction de la requête
+                            $lInstructionSql = "INSERT INTO posts "
+                                    . "(id, user_id, content, created, parent_id) "
+                                    . "VALUES (NULL, "
+                                    . $authorId . ", "
+                                    . "'" . $postContent . "', "
+                                    . "NOW(), "
+                                    . "NULL);";
+                            //Execution
+                            $ok = $mysqli->query($lInstructionSql);
+                            if (!$ok) {
+                                echo "Impossible d'ajouter le message: " . $mysqli->error;
+                            } else {
+                                echo "Message posté avec succès.";                            }
+                        } else {
+                            // sinon, affiche les erreurs (manque un email ou pseudo ou mot de passe)
+                            foreach ($erreurs as $erreur) {
+                                echo "<p style='color: red;'>$erreur</p>";
+                            }
+                        }
+                    }
+
+                    ?>
+                    <form action="wall.php" method="post">
+                        <input type="hidden" name="auteur" value="<?php echo $_SESSION['connected_id']; ?>">
+                        <dl>
+                            <dt><label for="message">Message</label></dt>
+                            <dd><textarea name="message" aria-describedby="message-error"></textarea></dd>
+                        </dl>
+                        <input type="submit">
+                    </form>
+
+
+                    <?php
                 $laQuestionEnSql = 
                 "SELECT posts.content, 
                     posts.created,    
