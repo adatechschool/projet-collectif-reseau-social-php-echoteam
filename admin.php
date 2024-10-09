0a9d45e1b3e052436c9e694a9048db4e3d262db4
Julien8387 <bellazini.julien@gmail.com>
2024-10-09 11:03:55 +0200

    ajout editor config + lien users page admin

--- admin.php
+++ admin.php
@@ -61,14 +61,15 @@ echo $head;
                         <h3><?php echo $tag['alias'] ?></h3>
                         <p><?php echo $tag['id'] ?></p>
                         <nav>
-                            <a href="wall.php?user_id=123">Mur</a>
-                            | <a href="feed.php?user_id=123">Flux</a>
-                            | <a href="settings.php?user_id=123">Paramètres</a>
-                            | <a href="followers.php?user_id=123">Suiveurs</a>
-                            | <a href="subscriptions.php?user_id=123">Abonnements</a>
+                          <a href="wall.php?user_id=<?php echo $tag['id']; ?>"  >Mur</a>;
+                          <a href="feed.php?user_id=<?php echo $tag['id']; ?>"  >Flux</a>;
+                          <a href="settings.php?user_id=<?php echo $tag['id']; ?>" >Paramètres</a>;
+                          <a href="followers.php?user_id=<?php echo $tag['id']; ?>"  >Suiveurs</a>;
+                          <a href="subscriptions.php?user_id=<?php echo $tag['id']; ?>" > Abonnements</a>;
                         </nav>
                     </article>
-                <?php } ?>
+                <?php 
+                } ?>
             </main>
         </div>
     </body>
